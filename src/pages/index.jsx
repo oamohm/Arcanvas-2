414ea210e69ec4c21ba47b0d0c5a32e9a17b66da
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-19 01:02:17 +0530

    Create index.jsx

---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      fontFamily: 'sans-serif',
+      backgroundColor: '#000',
+      color: '#fff'
+    }}>
+      <h1>Arcanvas - Live</h1>
+    </div>
+  );
+}
+
