13b21818075d1889e57687e638da3ec5f26cbba5
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-19 21:03:41 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
-module.exports = nextConfig
-
+  poweredByHeader: false,
+  env: {
+    NEXT_PUBLIC_ARC_CHAIN_ID: '2222',
+    NEXT_PUBLIC_ARC_RPC: 'https://rpc.testnet.arc.io',
+    NEXT_PUBLIC_ARCSCAN: 'https://testnet.arcscan.app',
+    NEXT_PUBLIC_USDC_ADDRESS: '0x0000000000000000000000000000000000000000',
+    NEXT_PUBLIC_ROYALTY_BPS: '50',
+  },
+  webpack(config, { isServer }) {
+    if (!isServer) config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+};
